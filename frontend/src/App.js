a7077a405d3cf4cd1cf11869c07cb8b8f74ec8ef
abalgir <abalgir@gmail.com>
2021-08-13 10:58:29 -0500

    controlled search componenet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 /* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
 
+
 function App() {
+  const[word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+    
+  }
+
+  console.log(word);
   return (
     <div className="App">
      <Header title="Images Gallery"/>
-     <Search />
+     <Search word={word} setWord={setWord} handleSubmit = {handleSearchSubmit}/>
     </div>
   );
 }
