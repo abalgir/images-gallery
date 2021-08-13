231c0d17f1a023967e3522b428ec272ccd8f88f8
abalgir <abalgir@gmail.com>
2021-08-12 19:21:04 -0500

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-unused-vars */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+
 
 function App() {
   return (
     <div className="App">
-     <h1> Images Gallery</h1>
+     <Header title="Images Gallery"/>
     </div>
   );
 }
